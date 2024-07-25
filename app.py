65d1b34f76ef4e5d8aac47ec742be4f74e3c8b36
landonweiqichamberlain <159598212+landonweiqichamberlain@users.noreply.github.com>
2024-07-25 12:55:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,24 @@ def handle_post():
         password = request.form['password']
         passwordConfirm = request.form['passwordconfirm']
         age = request.form['age']
-        validated = validate_input(username, password, passwordConfirm, email, age)
+        validated = Validation(username, password, passwordConfirm, email, age)
         if validated:
             if age == "":
                 age = -1
             user = [email, password, username, age]
+            
+            
+            select_data = "SELECT * FROM registered_users"
+            data = execute_read_query(connection, select_data)
 
-
+            for row in data:
+                if row[1] == email or row[3] == username:
+                    return render_template("index.html", title="Register")            
+            
             insert_query = (
                 f"INSERT INTO registered_users (email, password, username, age) VALUES {email, password, username, age}"
             )
+            connection.rollback()
             connection.autocommit = True
             cursor = connection.cursor()
             cursor.execute(insert_query, user)
@@ -43,25 +51,41 @@ def handle_post():
         
     else:
         return render_template('index.html')
-    
 
-def validate_input(Username, Password, PasswordConfirm, Email, Age):
-    z = 1
-    while z == 1:
-        if len(Username) < 21: z = 2
-    y = 1
-    while y == 1: 
-        if len(Password) > 7 and len(Password) < 21 and any(x.isupper() for x in Password) and any(not x.isalnum() for x in Password): y = 2
-    e = 1
-    while e == 1:
-        if Age == "": e = 2
-        elif int(Age) > 0: e = 2
-    h = 1
-    while h == 1:
-        if Email.endswith("@gmail.com" or "@hotmail.com" or "@outlook.com" or "@yahoo.com" or "@icloud.com"): h = 2
-    if PasswordConfirm == Password:
-        if z == 2 and y == 2 and e == 2 and h == 2:
-            return True
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    
+class Validation():
+    def __init__(self, Username, Password, PasswordConfirm, Email, Age):
+        self.Username = Username
+        self.Password = Password
+        self.PasswordConfirm = PasswordConfirm
+        self.Email = Email
+        self.Age = Age
+    def validate_input(self):
+        z = 1
+        while z == 1:
+            if len(self.Username) < 21: z = 2
+        y = 1
+        while y == 1: 
+            if len(self.Password) > 7 and len(self.Password) < 21 and any(x.isupper() for x in self.Password) and any(not x.isalnum() for x in self.Password): y = 2
+        e = 1
+        while e == 1:
+            if self.Age == "": e = 2
+            elif int(self.Age) > 0: e = 2
+        h = 1
+        while h == 1:
+            if self.Email.endswith("@gmail.com" or "@hotmail.com" or "@outlook.com" or "@yahoo.com" or "@icloud.com"): h = 2
+        if self.PasswordConfirm == self.Password:
+            if z == 2 and y == 2 and e == 2 and h == 2:
+                return True
     
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
